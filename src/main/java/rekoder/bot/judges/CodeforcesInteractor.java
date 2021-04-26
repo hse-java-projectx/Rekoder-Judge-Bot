1dc37b19f4d18821d34df1b6f00f061f4598d9c0
Glebanister <glebmar2001@gmail.com>
2021-04-26 19:45:21 +0300

    Implement codeforces problem parsing

---
+++ src/main/java/rekoder/bot/judges/CodeforcesInteractor.java
@@ -0,0 +1,58 @@
+package rekoder.bot.judges;
+
+import org.jsoup.*;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import rekoder.primitive.Problem;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.List;
+
+public class CodeforcesInteractor implements JudgeInteractor {
+    @Override
+    public String getName() {
+        return "Codeforces";
+    }
+
+    @Override
+    public List<Problem> getAllProblems() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Problem getProblemByUrl(String url) throws IOException {
+        Document document = Jsoup.connect(url)
+                .get();
+        Element statement = document.getElementsByClass("problem-statement")
+                .stream()
+                .findAny()
+                .orElseThrow();
+        String problemName = statement.getElementsByClass("title")
+                .stream()
+                .findAny()
+                .orElseThrow()
+                .text()
+                .chars()
+                .dropWhile(c -> c != ' ')
+                .skip(1)
+                .collect(StringBuilder::new,
+                        StringBuilder::appendCodePoint,
+                        StringBuilder::append)
+                .toString();
+        String problemStatement = statement.getElementsByTag("div")
+                .stream()
+                .limit(12)
+                .skip(11)
+                .findAny()
+                .orElseThrow()
+                .text();
+
+        return new Problem(problemName, problemStatement);
+    }
+
+    @Override
+    public List<String> getProblemsInInterval(DateFormat begin, DateFormat end) {
+        return null;
+    }
+}
