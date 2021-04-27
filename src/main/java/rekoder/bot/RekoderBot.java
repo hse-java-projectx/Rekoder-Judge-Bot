99761072114f19bbcbf30d670aa05cb818484a04
Glebanister <glebmar2001@gmail.com>
2021-04-27 14:52:33 +0300

    Remove redundant Wrapper Status enum

--- src/main/java/rekoder/bot/RekoderBot.java
+++ src/main/java/rekoder/bot/RekoderBot.java
@@ -43,11 +43,7 @@ public class RekoderBot implements Runnable {
         this.logger = logger;
         this.interactors = new HashMap<>();
         for (JudgeInteractor interactor : interactors) {
-            this.interactors.put(interactor.getName(), new JudgeInteractorWrapper(
-                    Timestamp.valueOf(LocalDateTime.MIN),
-                    interactor,
-                    JudgeInteractorStatus.NEVER_UPDATED
-            ));
+            this.interactors.put(interactor.getName(), new JudgeInteractorWrapper(interactor));
         }
     }
 
@@ -106,57 +102,6 @@ public class RekoderBot implements Runnable {
         }
     }
 
-    private class UpdateHandler implements CommandLineInterface.CliHandler {
-        @Override
-        public ResultOrError<String> handle(String[] args) {
-            if (args.length < 1) {
-                return new ResultOrError<>(Level.INFO, "Judge name is required as first argument");
-            }
-            String judgeName = args[0];
-            if (!interactors.containsKey(judgeName)) {
-                return new ResultOrError<>(Level.INFO, "Judge does not exist: '" + judgeName + "'");
-            }
-            JudgeInteractor interactor = interactors.get(judgeName).interactor;
-            Timestamp lastUpdate = interactors.get(judgeName).lastUpdate;
-            assert interactors.get(judgeName) != null;
-
-            tasks.add(() -> {
-                try {
-                    Timestamp curTime = Timestamp.valueOf(LocalDateTime.now());
-                    List<Problem> problemList = interactor.getProblemsInInterval(lastUpdate, curTime);
-                    interactors.put(judgeName, new JudgeInteractorWrapper(curTime, interactor, JudgeInteractorStatus.UPDATED));
-                    problemList.forEach(problem -> tasks.add(() -> {
-                        synchronized (api) {
-                            api.addProblem(problem, getErrorLoggingCallback());
-                        }
-                    }));
-                } catch (IOException e) {
-                    logger.log(Level.WARNING, String.format("Update was not successful: %s", e.getMessage()));
-                } catch (UnsupportedOperationException e) {
-                    logger.log(
-                            Level.INFO,
-                            String.format("Interactor '%s' does not support this operation, stack trace:\n%s",
-                                    interactor.getName(),
-                                    Arrays.stream(e.getStackTrace())
-                                            .map(StackTraceElement::toString)
-                                            .collect(Collectors.joining("\n"))));
-                }
-            });
-
-            return new ResultOrError<>("");
-        }
-
-        @Override
-        public String getHelp() {
-            return "Fetch recent problems from judge";
-        }
-
-        @Override
-        public List<String> getParams() {
-            return List.of("judge name");
-        }
-    }
-
     private Consumer<String> getErrorLoggingCallback() {
         return error -> logger.log(Level.WARNING, "Error callback: " + error);
     }
@@ -201,24 +146,72 @@ public class RekoderBot implements Runnable {
         }
     }
 
-    private enum JudgeInteractorStatus {
-        NEVER_UPDATED,
-        UPDATED
+    private class UpdateHandler implements CommandLineInterface.CliHandler {
+        @Override
+        public ResultOrError<String> handle(String[] args) {
+            if (args.length < 1) {
+                return new ResultOrError<>(Level.INFO, "Judge name is required as first argument");
+            }
+            String judgeName = args[0];
+            if (!interactors.containsKey(judgeName)) {
+                return new ResultOrError<>(Level.INFO, "Judge does not exist: '" + judgeName + "'");
+            }
+            JudgeInteractor interactor = interactors.get(judgeName).interactor;
+            Timestamp lastUpdate = interactors.get(judgeName).lastUpdate;
+            assert interactors.get(judgeName) != null;
+
+            tasks.add(() -> {
+                try {
+                    Timestamp curTime = Timestamp.valueOf(LocalDateTime.now());
+                    List<Problem> problemList = interactor.getProblemsInInterval(lastUpdate, curTime);
+                    interactors.get(judgeName).update(curTime);
+                    problemList.forEach(problem -> tasks.add(() -> {
+                        synchronized (api) {
+                            api.addProblem(problem, getErrorLoggingCallback());
+                        }
+                    }));
+                } catch (IOException e) {
+                    logger.log(Level.WARNING, String.format("Update was not successful: %s", e.getMessage()));
+                } catch (UnsupportedOperationException e) {
+                    logger.log(
+                            Level.INFO,
+                            String.format("Interactor '%s' does not support this operation, stack trace:\n%s",
+                                    interactor.getName(),
+                                    Arrays.stream(e.getStackTrace())
+                                            .map(StackTraceElement::toString)
+                                            .collect(Collectors.joining("\n"))));
+                }
+            });
+
+            return new ResultOrError<>("");
+        }
+
+        @Override
+        public String getHelp() {
+            return "Fetch recent problems from judge";
+        }
+
+        @Override
+        public List<String> getParams() {
+            return List.of("judge name");
+        }
     }
 
     private static class JudgeInteractorWrapper {
-        public final Timestamp lastUpdate;
+        public Timestamp lastUpdate;
         public final JudgeInteractor interactor;
-        public JudgeInteractorStatus status;
 
-        public JudgeInteractorWrapper(Timestamp lastUpdate, JudgeInteractor interactor, JudgeInteractorStatus status) {
-            this.lastUpdate = lastUpdate;
+        public JudgeInteractorWrapper(JudgeInteractor interactor) {
+            this.lastUpdate = Timestamp.valueOf(LocalDateTime.MIN);
             this.interactor = interactor;
-            this.status = status;
+        }
+
+        public void update(Timestamp time) {
+            this.lastUpdate = time;
         }
 
         public String getUpdateString() {
-            if (status == JudgeInteractorStatus.NEVER_UPDATED) {
+            if (lastUpdate.equals(Timestamp.valueOf(LocalDateTime.MIN))) {
                 return "never";
             }
             return lastUpdate.toString();
